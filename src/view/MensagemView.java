c2467b8e31fe06d58575d09a9671cd300a58aa75
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2026-06-01 21:48:11 -0300

    Create MensagemView.java

---
+++ src/view/MensagemView.java
@@ -0,0 +1,9 @@
+package view;
+
+public class MensagemView {
+    public void exibirSucesso(String mensagem) {
+        System.out.println("\n🖥️ [VIEW] ---------------------------------------");
+        System.out.println("SUCESSO: " + mensagem);
+        System.out.println("------------------------------------------------\n");
+    }
+}
